703a65513f9cd87abbe21372cad811caf3d1e22e
Eric Kohler <erickohler1600@gmail.com>
2022-05-23 22:55:30 -0400

    voter db app

---
+++ server.js
@@ -0,0 +1,26 @@
+const db = require('./db/connection');
+const express = require('express');
+const inputCheck = require('./utils/inputCheck');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Add near the top of the file
+const apiRoutes = require('./routes/apiRoutes');
+
+// Add after Express middleware
+app.use('/api', apiRoutes);
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
